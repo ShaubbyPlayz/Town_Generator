cf44e6007e9135db73867cf604476a7b469d5cea
ShaubbyPlayz <52765209+ShaubbyPlayz@users.noreply.github.com>
2021-09-27 17:59:40 -0500

    Names prt 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import {generateName} from "./utils.mjs";
+//var names = JSON.parse("./names.json");
+
+//console.log(names[2]);
 
 var name = [["a", "b", "c"], ["d", "e", "f"]];
-console.log(generateName(name)); 
+console.log(generateName(name));
