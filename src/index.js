f9a84f3dcd0f075cfd32733b9b6749d1678117c2
ShaubbyPlayz <52765209+ShaubbyPlayz@users.noreply.github.com>
2021-09-27 16:52:49 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import "./styles.css";
 import {generateName} from "./utils.mjs";
 
 var name = [["a", "b", "c"], ["d", "e", "f"]];
-console.log(generateName(name));
+console.log(generateName(name)); 
