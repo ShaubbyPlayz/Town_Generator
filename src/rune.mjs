2d592e5e5dd7726b857c2e80eccc4863e9a063a6
ShaubbyPlayz <52765209+ShaubbyPlayz@users.noreply.github.com>
2021-09-28 17:56:32 -0500

    rune generate transfer 2

--- src/rune.mjs
+++ src/rune.mjs
@@ -13,14 +13,14 @@ export function Rune() {
   // ----- Generate Corners -----
 
   runeMap[0][0] = coin(5/7); // Top Left
-  runeMap[0][3] = coin(5/7); // Top Middle
-  runeMap[0][5] = coin(6/7); // Top Right
-  runeMap[3][0] = coin(5/7); // Middle Left
-  runeMap[3][3] = coin(6/7); // Middle Middle
-  runeMap[3][5] = coin(6/7); // Middle Right
-  runeMap[5][0] = coin(4/7); // Bottom Left
-  runeMap[5][3] = coin(5/7); // Bottom Middle
-  runeMap[5][5] = coin(3/7); // Bottom Right
+  runeMap[0][2] = coin(5/7); // Top Middle
+  runeMap[0][4] = coin(6/7); // Top Right
+  runeMap[2][0] = coin(5/7); // Middle Left
+  runeMap[2][2] = coin(6/7); // Middle Middle
+  runeMap[2][4] = coin(6/7); // Middle Right
+  runeMap[4][0] = coin(4/7); // Bottom Left
+  runeMap[4][2] = coin(5/7); // Bottom Middle
+  runeMap[4][4] = coin(3/7); // Bottom Right
 
 
   // Returns
