6d851dea8f0f76093fd930ed597d0fa5d79b545c
ShaubbyPlayz <52765209+ShaubbyPlayz@users.noreply.github.com>
2021-10-01 11:15:24 -0500

    Rune Generator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
   // ----- IMPORTS -----
 import "./styles.css";
 import {coin, generateName} from "./utils.mjs";
-import {Rune} from "./rune.mjs";
+import {Rune, drawRune} from "./rune.mjs";
 import configData from './names.json';
 
 // Init Vars
@@ -11,35 +11,4 @@ var ctx = canvas.getContext("2d");
 // Import name.json
 var names = JSON.parse(JSON.stringify(configData));
 
-// ---------- RUNE GENERATOR ----------
-
-// Rune
-var rune = Rune();
-
-// Begin Path
-ctx.beginPath();
-
-// ----- Corners -----
-
-// Top
-if(rune[0][0] === 1) ctx.fillRect(0, 0, 10, 10); // Top Left
-if(rune[0][2] === 1) ctx.fillRect(30, 0, 10, 10); // Top Middle
-if(rune[0][4] === 1) ctx.fillRect(60, 0, 10, 10); // Top Right
-
-// Middle
-if(rune[2][0] === 1) ctx.fillRect(0, 30, 10, 10); // Middle Left
-if(rune[2][2] === 1) ctx.fillRect(30, 30, 10, 10); // Middle Middle
-if(rune[2][4] === 1) ctx.fillRect(60, 30, 10, 10); // Middle Right
-
-// Bottom
-if(rune[4][0] === 1) ctx.fillRect(0, 60, 10, 10); // Bottom Left
-if(rune[4][2] === 1) ctx.fillRect(30, 60, 10, 10); // Bottom Middle
-if(rune[4][4] === 1) ctx.fillRect(60, 60, 10, 10); // Bottom Right
-
-// ----- Horizontal Connectors -----
-
-if(rune[0][1] == 1) ctx.fillRect(10,)
-
-
-
-ctx.closePath();
+drawRune(ctx, 0, 0);
--- src/rune.mjs
+++ src/rune.mjs
@@ -16,11 +16,22 @@ function checkConnector(row, column, runeMap, chance) {
       return 0;
     }
 
-  } else { // Vertical
-    return 0;
+  } else if (row == 1 || row == 3){ // Vertical
+
+    // Check runeMap
+    if(runeMap[row + 1][column] == 1 && runeMap[row - 1][column] == 1) {
+      // Return if possible
+      return coin(chance);
+    } else {
+      // Return is not possible
+      return 0;
+    }
+
   }
 
 }
+
+
 
 // ---------- RUNE FUNCTION ----------
 export function Rune() {
@@ -49,16 +60,82 @@ export function Rune() {
   // ----- Generate Connectors -----
 
   // --- Horizontal ---
-  runeMap[0][1] = checkConnector(0, 1, runeMap, 1/2); // Top Left
-  runeMap[0][3] = coin(); // Top Right
+  runeMap[0][1] = checkConnector(0, 1, runeMap, 3/4); // Top Left
+  runeMap[0][3] = checkConnector(0, 3, runeMap, 3/4); // Top Right
   
-  runeMap[1][1] = coin(); // Middle Left
-  runeMap[1][2] = coin(); // Middle Right
+  runeMap[2][1] = checkConnector(2, 1, runeMap, 3/4); // Middle Left
+  runeMap[2][3] = checkConnector(2, 3, runeMap, 3/4); // Middle Right
 
-  runeMap[3][1] = coin(); // Bottom Left
-  runeMap[3][2] = coin(); // Bottom Right
+  runeMap[4][1] = checkConnector(4, 1, runeMap, 3/4); // Bottom Left
+  runeMap[4][3] = checkConnector(4, 3, runeMap, 3/4); // Bottom Right
+
+  // --- Vertical ---
+  runeMap[1][0] = checkConnector(1, 0, runeMap, 3/4); // Top Left
+  runeMap[1][2] = checkConnector(1, 2, runeMap, 3/4); // Top Middle
+  runeMap[1][4] = checkConnector(1, 4, runeMap, 3/4); // Top Right
+
+  runeMap[3][0] = checkConnector(3, 0, runeMap, 3/4); // Bottom Left
+  runeMap[3][2] = checkConnector(3, 2, runeMap, 3/4); // Bottom Middle
+  runeMap[3][4] = checkConnector(3, 4, runeMap, 3/4); // Bottom Right
 
   // Returns
   return runeMap;
+
+}
+
+// ---------- RUNE DRAWER ----------
+export function drawRune(ctx, xoffset, yoffset) {
+
+  // Rune
+  var rune = Rune();
+
+  // Begin Path
+  ctx.beginPath();
+
+  // ----- Corners -----
+  
+  // Top
+  if(rune[0][0] === 1) ctx.fillRect(0 + xoffset, 0 + yoffset, 10, 10); // Top Left
+  if(rune[0][2] === 1) ctx.fillRect(25 + xoffset, 0 + yoffset, 10, 10); // Top Middle
+  if(rune[0][4] === 1) ctx.fillRect(50 + xoffset, 0 + yoffset, 10, 10); // Top Right
+
+  // Middle
+  if(rune[2][0] === 1) ctx.fillRect(0 + xoffset, 30 + yoffset, 10, 10); // Middle Left
+  if(rune[2][2] === 1) ctx.fillRect(25 + xoffset, 30 + yoffset, 10, 10); // Middle Middle
+  if(rune[2][4] === 1) ctx.fillRect(50 + xoffset, 30 + yoffset, 10, 10); // Middle Right
+
+  // Bottom
+  if(rune[4][0] === 1) ctx.fillRect(0 + xoffset, 60 + yoffset, 10, 10); // Bottom Left
+  if(rune[4][2] === 1) ctx.fillRect(25 + xoffset, 60 + yoffset, 10, 10); // Bottom Middle
+  if(rune[4][4] === 1) ctx.fillRect(50 + xoffset, 60 + yoffset, 10, 10); // Bottom Right
+
+  // ----- Horizontal Connectors -----
+
+  // Top
+  if(rune[0][1] === 1) ctx.fillRect(10 + xoffset, 0 + yoffset, 15, 10); // Top Left
+  if(rune[0][3] === 1) ctx.fillRect(35 + xoffset, 0 + yoffset, 15, 10); // Top Right
+
+  // Middle
+  if(rune[2][1] === 1) ctx.fillRect(10 + xoffset, 30 + yoffset, 15, 10); // Top Left
+  if(rune[2][3] === 1) ctx.fillRect(35 + xoffset, 30 + yoffset, 15, 10); // Top Right
+
+  // Bottom
+  if(rune[4][1] === 1) ctx.fillRect(10 + xoffset, 60 + yoffset, 15, 10); // Top Left
+  if(rune[4][3] === 1) ctx.fillRect(35 + xoffset, 60 + yoffset, 15, 10); // Top Right
+
+  // ----- Vertical Connectors -----
+
+  // Top
+  if(rune[1][0] === 1) ctx.fillRect(0 + xoffset, 10 + yoffset, 10, 20); // Top Left
+  if(rune[1][2] === 1) ctx.fillRect(25 + xoffset, 10 + yoffset, 10, 20); // Top Middle
+  if(rune[1][4] === 1) ctx.fillRect(50 + xoffset, 10 + yoffset, 10, 20); // Top Left
+
+  // Bottom
+  if(rune[3][0] === 1) ctx.fillRect(0, + xoffset, 40 + yoffset, 10, 20); // Bottom Right
+  if(rune[3][2] === 1) ctx.fillRect(25 + xoffset, 40 + yoffset, 10, 20); // Bottom Middle
+  if(rune[3][4] === 1) ctx.fillRect(50 + xoffset, 40 + yoffset, 10, 20); // Bottom Left
+
+  // Close Path
+  ctx.closePath();
 
 }
